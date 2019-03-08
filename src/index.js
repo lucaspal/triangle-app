9239d330ee68c1d9d894eb3ef0e23e243affd0d6
Luca Spalierno <spalierno@gmail.com>
2019-03-08 18:46:40 +0100

    Initial commit. Basic project structure in place.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ts from '@tradeshift/tradeshift-ui';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// To make sure that all the tradeshift-ui components are initialized before rendering the page. Compliant to the tradeshift guideline.
+window.ts.ui.ready(function() {
+	ReactDOM.render(<App />, document.getElementById('root'));
+});
